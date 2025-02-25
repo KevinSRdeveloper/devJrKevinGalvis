c8300e286c47a01cf40567feb4d49ef278254ef8
Kevin David Zambrano Galvis <kevin.zambrano@aerocivil.gov.co>
2025-02-24 19:23:27 -0500

    form

--- src/pages/contactame.jsx
+++ src/pages/contactame.jsx
@@ -30,7 +30,30 @@ const ContactamePage = () => {
                 <div className="title-form">
                     <h2>Contactame!!</h2>
                 </div>
-
+               
+                <form action="" className="form">
+                    <div className="grupos-form-items">
+                        <label htmlFor="">Nombre Completo</label>
+                        <input type="text" name="" id="" />
+                    </div>
+                    <div className="grupos-form-items">
+                        <label htmlFor="">Correo Electronico</label>
+                        <input type="email" name="" id="" />
+                    </div>
+                    <div className="grupos-form-items">
+                        <label htmlFor="">Telefono</label>
+                        <input type="number" name="" id="" />
+                    </div>
+                    <div className="grupos-form-items">
+                        <label htmlFor="">Mensaje</label>
+                        <textarea name="" id=""></textarea>
+                    </div>
+                    <div className="grupos-form-buttons">
+                        <button type="submit">Enviar</button>
+                        <button type="button">Limpiar</button>
+                    </div>
+                </form>
+               
             </div>
            
 
